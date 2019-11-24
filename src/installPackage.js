2fd929b6090e39884096c087fb7feee1dc42f8f7
Richard Kozubal <4227791+Sepichat@users.noreply.github.com>
2019-11-24 13:56:29 +0100

    feat: installs package and logs missing package

--- src/installPackage.js
+++ src/installPackage.js
@@ -1,22 +1,45 @@
 const rimraf = require('rimraf');
 const path = require('path')
 const mkdir = require('mkdir-promise')
+const { exec } = require('child_process');
 const {workspacePath} = require('./config');
 
 const InstallPackage = {
+    getPath(packageName) {
+        return path.join(workspacePath, packageName)
+    },
+
     cleanUpPostBuild(packageName) {
-        const installPath = path.join(workspacePath, packageName);
+        const installPath = this.getPath(packageName);
         rimraf(installPath, () => {});
+        console.log('clean up done;')
     },
 
     async prepareWorkspace(packageName) {
-        const installPath = path.join(workspacePath, packageName);
+        const installPath = this.getPath(packageName);
         await mkdir(installPath);
-        console.log('workspace created')
-    }
+        console.log('workspace created');
+    },
+
+    async installPackage(packageName) {
+        await InstallPackage.prepareWorkspace(packageName);
+        installCommand = `npm install ${packageName}`;
+        exec(installCommand, {
+                cwd: this.getPath(packageName)
+            },
+            (err, stdout, stderr) => {
+                if(err) {
+                    console.log(stderr);
+                    InstallPackage.cleanUpPostBuild(packageName);
+                } else {
+                    console.log('Result command: ', stdout)
+                }
+            }
+        );
+    },
 }
 
 module.export = InstallPackage;
 
-InstallPackage.prepareWorkspace('test');
-InstallPackage.cleanUpPostBuild('test')
+// InstallPackage.installPackage('mukiyodaplop'); // fail
+InstallPackage.installPackage('react'); // OK
